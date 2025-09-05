390759c9bc109bb05c7025f55fe655ac57b04edd
Aliaksandr_Kreyer <Aliaksandr_Kreyer@epam.com>
2025-09-05 13:10:48 +0300

    Update the userDb

--- ticket-booking/src/main/java/ua/epam/mishchenko/ticketbooking/service/impl/DbTicketServiceImpl.java
+++ ticket-booking/src/main/java/ua/epam/mishchenko/ticketbooking/service/impl/DbTicketServiceImpl.java
@@ -2,9 +2,11 @@ package ua.epam.mishchenko.ticketbooking.service.impl;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import ua.epam.mishchenko.ticketbooking.model.Event;
 import ua.epam.mishchenko.ticketbooking.model.Ticket;
 import ua.epam.mishchenko.ticketbooking.model.User;
+import ua.epam.mishchenko.ticketbooking.repository.DbTicketRepository;
 import ua.epam.mishchenko.ticketbooking.service.TicketService;
 
 import java.util.List;
@@ -12,6 +14,9 @@ import java.util.List;
 public class DbTicketServiceImpl implements TicketService {
 
     private static final Logger LOGGER = LogManager.getLogger(DbTicketServiceImpl.class);
+
+    @Autowired
+    private DbTicketRepository dbTicketRepository;
 
     @Override
     public Ticket bookTicket(long userId, long eventId, int place, Ticket.Category category) {
